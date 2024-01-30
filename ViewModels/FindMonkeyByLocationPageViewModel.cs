8f556f7b4551ae0d4ccd445c513d18eebe41b3a3
User <User@LAB3-3>
2024-01-30 14:55:02 +0200

    kjfdkjfdkjfd

--- ViewModels/FindMonkeyByLocationPageViewModel.cs
+++ ViewModels/FindMonkeyByLocationPageViewModel.cs
@@ -12,12 +12,24 @@ namespace MonkeysMVVM.ViewModels
     {
         private string country;
         private int count;
-        public string Country {  get { return country; } set {  country = value; OnPropertyChanged(); } }
+        public string Country {  get { return country; } set {  country = value; OnPropertyChanged(); ((Command)SearchByCountryCommand).ChangeCanExecute(); } }
         public int Count { get { return count; } set { count = value; OnPropertyChanged(); } }
         public ICommand SearchByCountryCommand { get; set; }
 
         private Monkey monkey;
         public string Name { get {  return monkey.Name; } }
         public string ImageUrl { get { return monkey.ImageUrl; } }
+
+        public FindMonkeyByLocationPageViewModel() 
+        { 
+            monkey= new Monkey() { Name="אין קופים כרגע"};
+            SearchByCountryCommand = new Command(FindMonkeys, () => Country!=null || String.IsNullOrEmpty(Country));
+        }
+
+
+        private void FindMonkeys()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
